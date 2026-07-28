0f9c47baaa4a64949b45571341fac5ab8cfcfa68
vyachiksvn <vyachiksvn@users.noreply.github.com>
2026-07-28 11:55:42 +0300

    feat: update desktop exports for Sprint 3

--- feature/desktop/__init__.py
+++ feature/desktop/__init__.py
@@ -1,9 +1,13 @@
 """Feature: Desktop (operator workplace)"""
 
+from feature.desktop.focus_mode import FocusMode, FocusModeController, FocusState
 from feature.desktop.main_window import MainWindow
 from feature.desktop.panels import (
+    CandidateCard,
     CandidatePanel,
+    ConfirmWorkflow,
     HistoryPanel,
+    IdentityCard,
     PhotoViewer,
     ProfilePanel,
     SettingsPanel,
@@ -11,15 +15,24 @@ from feature.desktop.panels import (
     WorkspacePanel,
 )
 from feature.desktop.queue import QueueModel
+from feature.desktop.session import RecognitionSession, SessionStats
 
 __all__ = [
+    "FocusMode",
+    "FocusModeController",
+    "FocusState",
     "MainWindow",
     "QueueModel",
+    "CandidateCard",
     "CandidatePanel",
+    "ConfirmWorkflow",
     "HistoryPanel",
+    "IdentityCard",
     "PhotoViewer",
     "ProfilePanel",
     "SettingsPanel",
     "StatusBar",
     "WorkspacePanel",
+    "RecognitionSession",
+    "SessionStats",
 ]
